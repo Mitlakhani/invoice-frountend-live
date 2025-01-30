f4f70bec0e5085470fe1e98edcb3a36832fc6a64
mitlakhani <mit.lakhani@swiftrut.com>
2025-01-31 04:50:31 +0530

    toster added

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import Swal from "sweetalert2";
 
 const Login = () => {
